6fd56aa898d358c8e3e97a175e1ea8d7f6571d05
Walmir Portal <walmir.portal-ext@viavarejo.com.br>
2022-09-08 00:24:52 -0300

    Add feign Client and update tests with Fixture

--- src/main/java/br/com/user/api/UserApiApplication.java
+++ src/main/java/br/com/user/api/UserApiApplication.java
@@ -5,13 +5,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-//@EnableMongoRepositories
-//@EnableMongoAuditing
 @EnableFeignClients
 @OpenAPIDefinition(info=@Info(
 		title="Users API",
